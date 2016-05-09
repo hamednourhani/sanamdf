a8069aefbbd2b0a753ad2f5e1a93dc4926c3689d
Hamed Nourhani <info@itstar.ir>
2016-05-09 17:48:05 +0430

    first commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<!-- ********************************************************************* -->
+	<!--****************** Site footer      ***********************************-->
+	<!-- ********************************************************************* -->
+
+
+			<?php get_sidebar('footer-first'); ?>
+			<?php get_sidebar('footer-second'); ?>
+
+			<div class="follow-links">
+				<ul>
+					<li>
+						<a href="#">
+							<i class="fa fa"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+			<footer class="site-footer">
+				<section class="layout">
+					<span class="site-credit"><?php echo __( "Sana © 2016. All rights reserved.","itstar"); ?></span>
+
+					<span class="site-designer">
+						<a href="http://karait.com"><?php echo __('Designed ','itstar');?></a>
+						<?php echo __('By Farakaranet ','itstar');?>
+					</span>
+				</section>
+			</footer> <!-- footer -->
+	
+	
+	
+
+	<!-- scrolltofixed menu -->
+	
+	
+		</div>
+
+		<?php wp_footer(); ?>
+	</body><!-- body -->
+</html><!-- html -->
