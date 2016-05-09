ffefc832c7d0b47c749ff7625b24a05a63d256dc
Hamed Nourhani <info@itstar.ir>
2016-05-09 18:14:03 +0430

    first commit

--- footer.php
+++ footer.php
@@ -7,10 +7,33 @@
 			<?php get_sidebar('footer-second'); ?>
 
 			<div class="follow-links">
+				<h3 class="follow-title">
+					<?php echo __('Follow US ','itstar');?>
+				</h3>
 				<ul>
 					<li>
 						<a href="#">
-							<i class="fa fa"></i>
+							<i class="fa fa-google-plus" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-facebook" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-twitter" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-pinterest-p" aria-hidden="true"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-linkedin" aria-hidden="true"></i>
 						</a>
 					</li>
 				</ul>
